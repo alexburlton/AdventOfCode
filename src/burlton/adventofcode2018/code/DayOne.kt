519ca6fdf3359376c10e37cb3a174e8e5e4fcadf
alexburlton <alexburlton@hotmail.com>
2018-12-03 13:18:21 +0000

    Completed Day 1

--- src/burlton/adventofcode2018/code/DayOne.kt
+++ src/burlton/adventofcode2018/code/DayOne.kt
@@ -4,13 +4,51 @@ import java.io.File
 
 fun main(args: Array<String>)
 {
-    val lineList = mutableListOf<String>()
+    calculateTotalFrequency()
+    calculateFirstRepeatedFrequency()
+}
 
+private fun calculateFirstRepeatedFrequency()
+{
+    val frequencies = readFrequencies()
 
-    File("Frequencies.txt").useLines { lines -> lines.forEach{lineList.add(it)}}
+    var foundDuplicate = false
+    var initialTotal = 0
+    val totals = mutableSetOf<Int>()
+    while (!foundDuplicate)
+    {
+        for (freq in frequencies)
+        {
+            initialTotal += freq
+            if (totals.contains(initialTotal))
+            {
+                foundDuplicate = true
+                break
+
+            }
+
+            totals.add(initialTotal)
+        }
+    }
+
+    println("1B: $initialTotal")
+}
+
+private fun calculateTotalFrequency()
+{
+    val frequencyList = readFrequencies()
 
     var frequencySum = 0
-    lineList.forEach{frequencySum += Integer.parseInt(it)}
+    frequencyList.forEach{frequencySum += it}
 
-    println(frequencySum)
+    println("1A: $frequencySum")
+}
+
+private fun readFrequencies() : MutableList<Int>
+{
+    val frequencyList = mutableListOf<Int>()
+
+    File("Frequencies.txt").useLines { lines -> lines.forEach{frequencyList.add(Integer.parseInt(it))}}
+
+    return frequencyList
 }
