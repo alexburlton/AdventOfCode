a22abb59dd37917ff071be1fc5cc525091ff87f8
alexburlton <alexburlton@hotmail.com>
2018-12-03 13:53:11 +0000

    2A
    Done 2A. Tidying up mistakenly committed directories

--- src/burlton/adventofcode2018/code/DayOne.kt
+++ src/burlton/adventofcode2018/code/DayOne.kt
@@ -6,6 +6,33 @@ fun main(args: Array<String>)
 {
     calculateTotalFrequency()
     calculateFirstRepeatedFrequency()
+
+    calculateBoxesCheckSum()
+}
+
+private fun calculateBoxesCheckSum()
+{
+    val boxList = readBoxes()
+
+    val boxesWithTwoCharsRepeated = boxList.filter {it -> hasRepeatedLetters(it, 2)}
+    val boxesWithThreeCharsRepeated = boxList.filter{it -> hasRepeatedLetters(it, 3)}
+
+    val checksum = boxesWithThreeCharsRepeated.size * boxesWithTwoCharsRepeated.size
+    println("2A: $checksum")
+}
+
+private fun hasRepeatedLetters(s : String, repeatsRequired : Int) : Boolean
+{
+    val charMap = mutableMapOf<Char, Int>()
+    s.forEach { incrementValue(charMap, it) }
+
+    return charMap.values.contains(repeatsRequired)
+}
+private fun <K> incrementValue(map : MutableMap<K, Int>, key : K)
+{
+    var current = map.getOrPut(key, {0})
+    current++
+    map[key] = current
 }
 
 private fun calculateFirstRepeatedFrequency()
@@ -51,4 +78,13 @@ private fun readFrequencies() : MutableList<Int>
     File("Frequencies.txt").useLines { lines -> lines.forEach{frequencyList.add(Integer.parseInt(it))}}
 
     return frequencyList
+}
+
+private fun readBoxes() : MutableList<String>
+{
+    val boxList = mutableListOf<String>()
+
+    File("2. Boxes").useLines{ lines -> lines.forEach{boxList.add(it)}}
+
+    return boxList
 }
